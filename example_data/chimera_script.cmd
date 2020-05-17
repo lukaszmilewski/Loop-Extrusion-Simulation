23ed4b057bdb9ffaa469def7f13f37b80ff4d95b
lukaszmilewski <61455426+lukaszmilewski@users.noreply.github.com>
2020-05-17 09:54:37 +0200

    example_data from mdsoft bitbucket

---
+++ example_data/chimera_script.cmd
@@ -0,0 +1,12 @@
+open 27-7x7_z50_2_nuc35-syg2.cmap
+open xyz_medium.bld
+
+~set singleLayer
+volume #0 level 200 level 14000 level 18000 color 0,1,0,0.08 color 1,1,0,0.19 color 1,0,0,0.3
+volume #0 voxelSize 0.49949,0.49949,3.5678
+volume #0 origin 0,0,0
+volume #0 showOutlineBox True
+focus
+
+#open minimized.pdb
+
