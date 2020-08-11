68d3da0eab87be3ceb14640125b4da3ed967494c
Elzagro <62719778+Elzagro@users.noreply.github.com>
2020-08-11 19:14:14 +0200

    Update md_utils.py

--- md_utils.py
+++ md_utils.py
@@ -2,7 +2,11 @@ import csv
 
 import numpy as np
 from matplotlib import pyplot as plt
-
+def gen_sin_array (N,F):
+    a=np.arange(0,N+1)
+    b=F*np.sin((a/(N/(np.pi/2))))
+    c=np.flip(b)
+    return np.array([[a],[b],[c]])
 
 def plot_data(state_fname, plot_fname):
     with open(state_fname) as csv_file:
